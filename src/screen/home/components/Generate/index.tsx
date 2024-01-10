19aebb2252e3d52d4c01e42459d7d603fda03517
wanpp <6653302wanpp@gmail.com>
2024-01-10 13:44:44 +0800

    feat: update ui

--- src/screen/home/components/Generate/index.tsx
+++ src/screen/home/components/Generate/index.tsx
@@ -1,13 +1,24 @@
-import { Button, Divider, Form, Input } from '@arco-design/web-react';
+import { Button, Divider, Input } from '@arco-design/web-react';
 import { open } from '@tauri-apps/api/dialog';
 import { downloadDir } from '@tauri-apps/api/path';
-import { FunctionComponent, ReactElement, useCallback, useEffect } from 'react';
+import { FunctionComponent, ReactElement, useCallback, useEffect, useState } from 'react';
 import { Parser } from '../../../../utils/Parser';
 
+interface Cache {
+    jsonUrl?: string;
+    savePath?: string;
+}
+
 const parser = new Parser();
-const cacheData = localStorage.getItem('cache');
+const cache = localStorage.getItem('cache');
+let cacheData = cache ? (JSON.parse(cache) as Cache) : ({} as Cache);
+console.log('cacheData: ', cacheData);
+const saveInLocalCache = () => {
+    localStorage.setItem('cache', JSON.stringify(cacheData));
+};
 export const Generate: FunctionComponent = (): ReactElement => {
-    const [form] = Form.useForm();
+    const [savePath, setSavePath] = useState(cacheData?.savePath || '');
+    const [jsonUrl, setJsonUrl] = useState(cacheData?.jsonUrl || '');
 
     const onSaveOutputDir = useCallback(async () => {
         const filePath = await open({
@@ -16,12 +27,21 @@ export const Generate: FunctionComponent = (): ReactElement => {
             title: '请选择文件导出目录',
             defaultPath: await downloadDir(),
         });
-        form.setFieldValue('outputDir', filePath || '');
-    }, [form]);
+        setSavePath((filePath as string) || '');
+        cacheData = { ...cacheData, savePath: filePath as string };
+        saveInLocalCache();
+    }, []);
+
+    const onSaveJsonUrl = useCallback((value: string) => {
+        setJsonUrl(value);
+        cacheData = { ...cacheData, jsonUrl: value };
+        saveInLocalCache();
+    }, []);
 
     const setDownloadDir = useCallback(async () => {
-        form.setFieldValue('outputDir', await downloadDir());
-    }, [form]);
+        if (savePath) return;
+        setSavePath((await downloadDir()) || '');
+    }, [savePath]);
 
     useEffect(() => {
         setDownloadDir();
@@ -32,46 +52,19 @@ export const Generate: FunctionComponent = (): ReactElement => {
             <p className="text-[--color-text-1] text-[16px] font-medium">生成代码文件</p>
             <Divider />
 
-            <Form
-                form={form}
-                onValuesChange={(_, values) => {
-                    console.log('onValuesChange: ', values);
-                    localStorage.setItem('cache', JSON.stringify(values));
-                }}
-                onSubmit={() => {
-                    console.log('onSubmit', form.getFields());
-                    parser.start(form.getFieldValue('jsonUrl'));
-                }}
-                initialValues={{
-                    outputDir: cacheData ? JSON.parse(cacheData)?.outputDir : '',
-                    jsonUrl: cacheData ? JSON.parse(cacheData)?.jsonUrl : '',
-                }}
-                scrollToFirstError
-            >
-                <Form.Item
-                    label="导出目录"
-                    // rules={[{ required: true }]}
-                    field="outputDir"
-                    className="flex items-center"
-                >
-                    <Input style={{ width: 420 }} value={form.getFieldValue('outputDir')} className=" mr-2 " />
-                    <Button onClick={onSaveOutputDir}>浏览</Button>
-                </Form.Item>
+            <div className="mb-4">
+                <span className=" mr-2 w-20 inline-block ">导出目录: </span>
+                <Input style={{ width: 420 }} value={savePath} className=" mr-2 " />
+                <Button onClick={onSaveOutputDir}>浏览</Button>
+            </div>
+            <div>
+                <span className=" mr-2 w-20 inline-block">JSON URL:</span>
+                <Input style={{ width: 420 }} className=" mr-2 " onChange={(value) => onSaveJsonUrl(value)} />
+            </div>
 
-                <Form.Item label="JSON URL:">
-                    <Input
-                        style={{ width: 420 }}
-                        className=" mr-2 "
-                        onChange={(value) => form.setFieldValue('jsonUrl', value)}
-                    />
-                </Form.Item>
-
-                <Form.Item>
-                    <Button htmlType="submit" type="primary">
-                        生成代码
-                    </Button>
-                </Form.Item>
-            </Form>
+            <Button type="primary" size="large" onClick={() => parser.start(jsonUrl)} className="mt-4">
+                生成
+            </Button>
         </div>
     );
 };
